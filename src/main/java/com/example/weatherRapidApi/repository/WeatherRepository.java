2cc8b8bd000c91ba26cb63881cc55e0f55e92134
StasOger <92465858+StasOger@users.noreply.github.com>
2023-12-04 02:02:05 +0300

    Finally

--- src/main/java/com/example/weatherRapidApi/repository/WeatherRepository.java
+++ src/main/java/com/example/weatherRapidApi/repository/WeatherRepository.java
@@ -10,7 +10,6 @@ import java.util.Date;
 @Repository
 public interface WeatherRepository extends JpaRepository<Weather, Integer> {
 
-//    @Query("SELECT w FROM Weather w ORDER BY w.date DESC LIMIT 1")
     Weather findFirstByOrderByDateDesc();
 
     @Query("SELECT AVG(w.tempc) FROM Weather w WHERE w.date BETWEEN ?1 AND ?2")
--- src/main/resources/weather.sql
+++ src/main/resources/weather.sql
@@ -19,4 +19,12 @@ SELECT AVG(w.tempc) FROM Weather w
 SELECT AVG(w.tempc) FROM Weather w WHERE w.date BETWEEN '2023-12-01 21:30' AND '2023-12-03 21:30'
 
 INSERT weather (tempc, maxwindmph, pressuremb, humidity, conditiontype, locationname, date)
+VALUES (-21.8, 19.7, 1004.0, 94.0, 'Heavy snow', 'Europe/Minsk', '2023-11-29 21:30');
+INSERT weather (tempc, maxwindmph, pressuremb, humidity, conditiontype, locationname, date)
+VALUES (-51.8, 19.7, 1004.0, 94.0, 'Heavy snow', 'Europe/Minsk', '2023-11-30 21:30');
+INSERT weather (tempc, maxwindmph, pressuremb, humidity, conditiontype, locationname, date)
 VALUES (-5.8, 19.7, 1004.0, 94.0, 'Heavy snow', 'Europe/Minsk', '2023-12-01 21:30');
+INSERT weather (tempc, maxwindmph, pressuremb, humidity, conditiontype, locationname, date)
+VALUES (-2.8, 19.7, 1004.0, 94.0, 'Heavy snow', 'Europe/Minsk', '2023-12-02 21:30');
+INSERT weather (tempc, maxwindmph, pressuremb, humidity, conditiontype, locationname, date)
+VALUES (-1.8, 19.7, 1004.0, 94.0, 'Heavy snow', 'Europe/Minsk', '2023-12-03 21:30');
